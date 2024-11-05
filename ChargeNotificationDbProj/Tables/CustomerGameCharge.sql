34c9cd27f84460a31b221e5e6d87e3eb7fa92c25
vitali <vliubarski@gmail.com>
2024-11-05 12:32:18 +0000

    added constraints for db integrity

--- ChargeNotificationDbProj/Tables/CustomerGameCharge.sql
+++ ChargeNotificationDbProj/Tables/CustomerGameCharge.sql
@@ -7,6 +7,9 @@
     [ChargeDate] DATE NOT NULL, 
     [CustomerId] INT NOT NULL, 
     CONSTRAINT [FK_CustomerGameCharge_ToTable] FOREIGN KEY ([CustomerId]) REFERENCES [Customer]([Id]), 
+    CONSTRAINT CHK_TotalCost_Positive CHECK ([TotalCost] > 0), 
+    CONSTRAINT [UQ_CustomerGameCharge_UniqueEntry] UNIQUE ([CustomerId], [GameName], [ChargeDate]), 
+    CONSTRAINT [FK_CustomerGameCharge_ToCustomer] FOREIGN KEY ([CustomerId]) REFERENCES [Customer]([Id]) ON DELETE CASCADE, 
 )
 
 GO
